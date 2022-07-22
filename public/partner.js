0536c643c4ac664474948e856f06da307eb02d87
zetria.org <service@zetria.org>
2022-07-22 15:03:18 +0800

    new stuff

--- public/partner.js
+++ public/partner.js
@@ -1,27 +1,20 @@
+import { getStorage, ref } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-storage.js';
+
 var p_cur = 1,
-    p_name = ["臺北醫學大學",
-    "臺北市信義區大道\n社區發展協會",
-    "台北市萬華區\n忠貞里中正新城\n社區發展協會",
-    "台北市北投區林泉\n社區發展協會",
-    "台北市文山區德芳\n社區發展協會",
-    "臺北市文山區樟新\n社區發展協會",
-    "社團法人\n中華基督教\n合一宣教協會",
-    "臺北市興隆老人\n日間照顧中心",
-    "佛光山大慈佛社\n",
-    "台北市文山區\n順興社區發展協會",
-    "臺北市文山區\n萬興里辦公室",
-    "臺北市文山區\n試院里辦公室",
-    "財團法人\n信義公益基金會",
-    "社團法人台南市\n基督教青年會",
-    "財團法人伊甸\n社會福利基金會\n(宜蘭社區服務中心)",
-    "台北市政府社會局\n文山景美社會福利\n服務中心",
-    "台北市文山區\n明興社區發展協會",
-    "財團法人基督教\n救世軍內湖隊",
-    "國立政治大學\n大學社會責任辦公室",
-    "屏東縣私立柏愛\n老人養護中心"],
+    p_name = [],
     len_div;
 
+const scanActivities = setInterval(() => {
+    if(window.activities){
+        p_name = window.activities;
+        partner_init();
+        partner_display();
+        clearInterval(scanActivities);
+    }
+}, 100);
+
 function partner_init(){
+    console.log(p_name);
     if($(window).width() > 768){
         len_div = 3;
         while(p_name.length % 3 != 0){
@@ -42,21 +35,26 @@ function partner_init(){
     }
 }
 
-partner_init();
-
-for(var i = 0; i < p_name.length; i++){
-    if(p_name[i] == ""){
-        $(".partner-block").append($('<div/>').addClass("partner-card"));
-    }
-    else{
-        $(".partner-block").append(
-            $('<div/>').addClass("partner-card").append(
-                [$('<div/>').addClass("partner-activity-img").css("background-image", "url(./img/activity/activity" + (i + 1) + ".jpg)"),
-                $('<p/>').text(p_name[i])]
-            )
-        );
+function partner_display(){
+    const storage = getStorage();
+    for(var i = 0; i < p_name.length; i++){
+        if(p_name[i] == ""){
+            $(".partner-block").append($('<div/>').addClass("partner-card"));
+        }
+        else{
+            const pathRef = ref(storage, `activities/${p_name[i].file}`);
+            $(".partner-block").append(
+                $('<div/>').addClass("partner-card").append(
+                    [$('<div/>').addClass("partner-activity-img").css("background-image", `url(${pathRef})`),
+                    $('<p/>').html(p_name[i].name.replace(/\\n/g, "<br />"))]
+                )
+            );
+        }
     }
 }
+
+partner_init();
+partner_display();
 
 $(".partner-block p").each(function(){
     if($(this).text().includes("\n")){
